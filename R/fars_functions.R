306ce969d40c27c9a87647c32ecdb4a59962ef5d
Grandez <javier.gonzalez.grandez@gmail.com>
2017-01-22 12:51:48 +0100

    Removed examples because they require data in cwd

--- R/fars_functions.R
+++ R/fars_functions.R
@@ -22,15 +22,14 @@
 #' @return The dataframe generated
 #'         Error if file doesn't exist
 #'
-#' @examples
-#' fars_read('filename')
-#' fars_read('dir1/dir2/filename')
 #'
 fars_read <- function(filename) {
-        if(!file.exists(filename))
+        fname <- filename
+        if(!file.exists(filename)) {
                 stop("file '", filename, "' does not exist")
+        }
         data <- suppressMessages({
-                readr::read_csv(filename, progress = FALSE)
+                readr::read_csv(fname, progress = FALSE)
         })
         dplyr::tbl_df(data)
 }
@@ -45,12 +44,12 @@ fars_read <- function(filename) {
 #' @return An string with the name generated
 #'         Error if year is not numeric
 #'
-#' @examples
-#' make_filename(2016)
+# #' @examples
+# #' make_filename(2013)
 #'
 make_filename <- function(year) {
         year <- as.integer(year)
-        sprintf("accident_%d.csv.bz2", year)
+        name <- sprintf("accident_%d.csv.bz2", year)
 }
 
 #' Print "fars_read_years"
@@ -64,11 +63,10 @@ make_filename <- function(year) {
 #' @return A list with an element for each file including MONTH and year
 #'         Error if doesn't exist the file associated to year
 #'
-#' @examples
-#' fars_read_years(2016)
-#' fars_read_years(c(2013,2014,2015))
+# #' @examples
+# #' fars_read_years(c(2013,2014,2015))
 #'
-
+# #' @export
 fars_read_years <- function(years) {
         lapply(years, function(year) {
                 file <- make_filename(year)
@@ -76,12 +74,13 @@ fars_read_years <- function(years) {
                         dat <- fars_read(file)
 
                         dplyr::mutate(dat, year = year)  %>%
-                                dplyr::select(MONTH, year)
+                                dplyr::select("MONTH", year)
                 }, error = function(e) {
-                        warning("invalid year: ", year)
-                        return(NULL)
+                        stop("Invalid year: ", year)
+#                        return(NULL)
                 })
         })
+
 }
 #' Print "fars_summarize_years"
 #'
@@ -93,31 +92,16 @@ fars_read_years <- function(years) {
 #'
 #' @return A data frame with the amount of accidents for year by month
 #'
-#' @examples
-#' fars_read_years(c(2013,2014,2015))
-#' return a data frame like this:
-#'    MONTH `2013` `2014` `2015`
-#'*  <int>  <int>  <int>  <int>
-#'1      1   2230   2168   2368
-#'2      2   1952   1893   1968
-#'3      3   2356   2245   2385
-#'4      4   2300   2308   2430
-#'5      5   2532   2596   2847
-#'6      6   2692   2583   2765
-#'7      7   2660   2696   2998
-#'8      8   2899   2800   3016
-#'9      9   2741   2618   2865
-#'10    10   2768   2831   3019
-#'11    11   2615   2714   2724
-#'12    12   2457   2604   2781
+# #' @examples
+# #' fars_summarize_years(c(2013,2014,2015))
 #'
 #' @export
 fars_summarize_years <- function(years) {
         dat_list <- fars_read_years(years)
         dplyr::bind_rows(dat_list) %>%
-                dplyr::group_by(year, MONTH) %>%
+                dplyr::group_by("year", "MONTH") %>%
                 dplyr::summarize(n = n()) %>%
-                tidyr::spread(year, n)
+                tidyr::spread("year", n)
 }
 
 #' Print "fars_map_state"
@@ -131,8 +115,8 @@ fars_summarize_years <- function(years) {
 #'
 #' @return A plot with the accidents occurred by latitude and longitude
 #'
-#' @examples
-#' fars_map_state(1,2013)
+# #' @examples
+# #' fars_map_state(1,2013)
 #'
 #' @export
 fars_map_state <- function(state.num, year) {
@@ -142,7 +126,7 @@ fars_map_state <- function(state.num, year) {
 
         if(!(state.num %in% unique(data$STATE)))
                 stop("invalid STATE number: ", state.num)
-        data.sub <- dplyr::filter(data, STATE == state.num)
+        data.sub <- dplyr::filter(data, "STATE" == state.num)
         if(nrow(data.sub) == 0L) {
                 message("no accidents to plot")
                 return(invisible(NULL))
